4804924a50008d423d7790a6ad809d225064f986
imanh79 <iman.h0089@gmail.com>
2024-05-02 22:48:58 +0330

    deployed1

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
-      <body className={inter.className}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={roboto.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme=""
