fb3c42739c1f9484e7bab94c760494af1434f3bc
imanh79 <iman.h0089@gmail.com>
2024-05-17 22:52:03 +0330

    deployed3

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div
       className={`grid grid-cols-[auto,1fr,1fr] overflow-clip relative h-screen`}
     >
-      <section className="col-start-1 col-end-2 bg-bgside h-screen">
+      <section className="col-start-1 col-end-2 bg-bgside h-full">
         <Sidebar />
       </section>
       <section className="col-start-2 col-end-4 h-screen overflow-clip">
