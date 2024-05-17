e21b77ff10d828c3d833df8905e35311804d036b
imanh79 <iman.h0089@gmail.com>
2024-05-17 21:24:44 +0330

    deployed3

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ import Sidebar from "@/components/sidebars/sidebar";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-[auto,1fr,1fr] h-screen overflow-hidden relative">
-      <section className="col-start-1 col-end-2 bg-bgside min-h-full">
+    <div
+      className={`grid grid-cols-[auto,1fr,1fr] overflow-clip relative h-screen`}
+    >
+      <section className="col-start-1 col-end-2 bg-bgside h-full">
         <Sidebar />
       </section>
-      <section className="col-start-2 col-end-4 overflow-hidden h-screen">
+      <section className="col-start-2 col-end-4 h-screen overflow-clip">
         <Main />
       </section>
-      <section className="col-start-4 col-end-6"></section>
+      <section className="col-start-4 col-end-6 overflow-clip"></section>
     </div>
   );
 }
